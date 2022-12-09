1c5d45a25d34f0d5681fd98493d989d0c81f6e0b
Marwa Nabi <marwa@Marwas-MacBook-Pro.local>
2022-12-10 01:27:55 +0600

    Fix twitter share

--- js/share-plugin.js
+++ js/share-plugin.js
@@ -3,6 +3,6 @@ var pageTitle = String(document.title).replace(/\&/g, '%26');
 
 function facebook_click() { window.open(`http://www.facebook.com/sharer.php?u=${pageLink}&quote=${pageTitle}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
 
-function twitter_click() { window.open(`https://twitter.com/intent/status?text=${pageTitle}&status=${pageLink}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
+function twitter_click() { window.open(`https://twitter.com/intent/tweet?text=${pageTitle}&status=${pageLink}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
 
 function linkedin_click() { window.open(`https://www.linkedin.com/sharing/share-offsite/?url=${pageLink}`, 'sharer', 'toolbar=0,status=0,width=626,height=436'); return false; }
